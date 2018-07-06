19e7be976b77d8074edfff261873700f8b138e42
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-07-06 15:20:41 +0800

    Add project files.

---
+++ DynamicProgramming/EqualDistribution.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class EqualDistribution
+    {
+        private static int min = -1;
+        // Complete the equal function below.
+        public int Equal(int[] intArray)
+        {
+            decimal difference = -1;
+            int max = intArray.Length - 1;
+            int count = 0;
+            do
+            {
+                Array.Sort(intArray);
+                difference = intArray[max] - intArray[0];
+                int numberToSubtract = 0;
+                if (difference > 2 && intArray[max] >= 5)
+                {
+                    numberToSubtract = 5;
+                }
+                else if (difference >= 2 && intArray[max] < 5)
+                {
+                    numberToSubtract = 2;
+                }
+                else if (difference == 2 && intArray[max] >= 5)
+                {
+                    numberToSubtract = 2;
+                }
+                else if (difference == 1)
+                {
+                    numberToSubtract = 1;
+                }
+                else
+                {
+                    break;
+                }
+
+                count++;
+
+                intArray[max] = intArray[max] - numberToSubtract;
+
+            } while (difference != 0);
+
+            return count;
+        }
+
+        public int MinRound(int[] counts)
+        {
+            for (int j = 0; j < counts.Length; j++)
+            {
+                // get min value from input array
+                if (counts[j] < min || min < 0)
+                {
+                    min = counts[j];
+                }
+            }
+
+            int[][] results = new int[counts.Length][];
+            for (int i = 0; i < counts.Length; i++)
+            {
+                results[i] = new int[3];
+                for (int j = 0; j < 3; j++)
+                {
+
+                    //Determine baselines -- difference between possible Max and Min
+                    //Not only possible to take from Max values also can take from Min values
+                    //Like the case  1 5 5  -> 0 5 5 -> 0 0 5 -> 0 0 0
+                    //Baseline can be keep min, take 1 or 2 from min. 
+                    //Dont need to consider the case of take 5 coz it will not affect greedy approach below
+
+                    int delta = counts[i] - min + j;
+                    results[i][j] = 0;
+                    while (true)
+                    {
+                        // Greedy approach
+                        if (delta >= 5)
+                        {
+                            delta -= 5;
+                            results[i][j]++;
+                        }
+                        else if (delta >= 2)
+                        {
+                            delta -= 2;
+                            results[i][j]++;
+                        }
+                        else if (delta >= 1)
+                        {
+                            delta -= 1;
+                            results[i][j]++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            int finalResult = -1;
+            // Compare results from different baseline cases (keep min, take 1, 2 ). 
+            for (int i = 0; i < 3; i++)
+            {
+                int subFinal = 0;
+                for (int j = 0; j < counts.Length; j++)
+                {
+                    subFinal += results[j][i];
+                    //if (DBG) System.out.format("results[%d][%d] = %d \n", j, i, results[j][i]);
+                }
+                //if (DBG) System.out.println(subFinal);
+                if (finalResult < 0 || finalResult > subFinal) { finalResult = subFinal; }
+            }
+
+            return finalResult;
+        }
+    }
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] intArray = { 2, 5, 5, 5, 5, 5 };
+            int[] intArray = { 1, 5, 5, 5, 5, 5 };
+            //int[] intArray = { 512, 125, 928, 381, 890 , 90, 512, 789, 469, 473, 908, 990, 195, 763, 102, 643, 458, 366, 684, 857, 126, 534, 974, 875, 459, 892, 686, 373, 127, 297, 576, 991, 774, 856, 372, 664, 946, 237, 806, 767, 62, 714, 758, 258, 477, 860, 253, 287, 579, 289, 496 };
+            //int[] intArray = { 858, 710 };
+            Console.WriteLine(new EqualDistribution().MinRound(intArray));
+            Console.Read();
+        }
+    }
+}
