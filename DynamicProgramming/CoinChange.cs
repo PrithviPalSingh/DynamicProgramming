f536b6d769db3387a090e5105c440428427cfddd
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-07-06 17:31:53 +0800

    Coin Change Problem

---
+++ DynamicProgramming/CoinChange.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    /// <summary>
+    /// Review this once again
+    /// https://www.youtube.com/watch?v=sn0DWI-JdNA
+    /// </summary>
+    class CoinChange
+    {
+        public long GetWays(int[] coins, int money)
+        {
+            Dictionary<string, long> memo = new Dictionary<string, long>();
+            var ways = GetWays(coins, money, 0, memo);
+            return ways;
+            //return GetWays(coins, money, 0);
+        }
+
+        private long GetWays(int[] coins, int money, int index, Dictionary<string, long> memo)
+        {
+            if (money == 0)
+                return 1;
+
+            if (index >= coins.Length)
+                return 0;
+
+            string key = money + "-" + index;
+
+            if (memo.ContainsKey(key))
+            {
+                return memo[key];
+            }
+
+            int amountWithCoin = 0;
+            long ways = 0;
+            while (amountWithCoin <= money)
+            {
+                int remaining = money - amountWithCoin;
+                ways += GetWays(coins, remaining, index + 1, memo);
+                amountWithCoin += coins[index];
+            }
+
+            memo.Add(key, ways);
+
+            return ways;
+        }
+
+        private long GetWays(int[] coins, int money, int index)
+        {
+            if (money == 0)
+                return 1;
+
+            if (index >= coins.Length)
+                return 0;
+
+            //string key = money + "-" + index;
+
+            //if (memo.ContainsKey(key))
+            //{
+            //    return memo[key];
+            //}
+
+            int amountWithCoin = 0;
+            long ways = 0;
+            while (amountWithCoin <= money)
+            {
+                int remaining = money - amountWithCoin;
+                ways += GetWays(coins, remaining, index + 1);
+                amountWithCoin += coins[index];
+            }
+
+            //memo.Add(key, ways);
+
+            return ways;
+        }
+    }
+}
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -11,10 +11,13 @@ namespace DynamicProgramming
         static void Main(string[] args)
         {
             //int[] intArray = { 2, 5, 5, 5, 5, 5 };
-            int[] intArray = { 1, 5, 5, 5, 5, 5 };
+            //int[] intArray = { 1, 5, 5, 5, 5, 5 };
             //int[] intArray = { 512, 125, 928, 381, 890 , 90, 512, 789, 469, 473, 908, 990, 195, 763, 102, 643, 458, 366, 684, 857, 126, 534, 974, 875, 459, 892, 686, 373, 127, 297, 576, 991, 774, 856, 372, 664, 946, 237, 806, 767, 62, 714, 758, 258, 477, 860, 253, 287, 579, 289, 496 };
             //int[] intArray = { 858, 710 };
-            Console.WriteLine(new EqualDistribution().MinRound(intArray));
+            //Console.WriteLine(new EqualDistribution().MinRound(intArray));
+
+            int[] coins = { 2, 5, 3, 6 };
+            Console.WriteLine(new CoinChange().GetWays(coins, 10));
             Console.Read();
         }
     }
