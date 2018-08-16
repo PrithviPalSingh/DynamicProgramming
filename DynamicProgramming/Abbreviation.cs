d356b8ccd2579fcfad3108379507006c3055bfc9
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-08-16 17:06:06 +0800

    Changes for Longest common subsequence

---
+++ DynamicProgramming/Abbreviation.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Abbreviation
+    {
+        public static string abbreviation1(string a, string b)
+        {
+            char[] a1 = a.ToCharArray();
+            var b1 = b.ToCharArray();
+            Dictionary<char, int> memo = new Dictionary<char, int>();
+            #region - remove unnecessary items
+            for (int i = a1.Length - 1; i >= 0; i--)
+            {
+                if (a1[i].ToString() == a1[i].ToString().ToUpper())
+                {
+                    continue;
+                }
+
+                var found = false;
+                for (int j = 0; j < b1.Length; j++)
+                {
+                    if (a1[i].ToString().ToUpper() == b[j].ToString())
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    var j = i;
+                    if (i == a1.Length - 1)
+                    {
+                        a1[i] = ' ';
+                    }
+                    else
+                    {
+                        while (j < a1.Length - 1 && a1[j] != ' ')
+                        {
+                            a1[j] = a1[j + 1];
+                            j++;
+                        }
+
+                        if (j != a1.Length)
+                            a1[j] = ' ';
+                    }
+                }
+            }
+            #endregion
+
+            //string str = string.Empty;
+            //for (int i = 0; i < a1.Length; i++)
+            //{
+            //    if (a1[i] != ' ')
+            //    {
+            //        str = str + a1[i].ToString();
+            //    }
+            //}
+
+            var up = a1;// str.ToCharArray();
+
+
+            for (int i = 0; i < b.Length && up[i] != ' '; i++)
+            {
+                //var j = i;
+
+                if (up[i].ToString() == up[i].ToString().ToUpper() && up[i].ToString() == b[i].ToString())
+                {
+                    continue;
+                }
+
+                if (up[i].ToString() != b[i].ToString() && up[i].ToString().ToUpper() == b[i].ToString())
+                {
+                    continue;
+                }
+
+
+                //if (b[i].ToString() != up[i].ToString().ToUpper() && up[j] != ' ')
+                //{
+                //    while (j < up.Length - 1)
+                //    {
+                //        up[j] = up[j + 1];
+                //        j++;
+                //    }
+
+                //    if (j != a1.Length)
+                //        up[j] = ' ';
+
+                //    i--;
+                //}
+
+                if (b[i].ToString() != up[i].ToString().ToUpper())
+                {
+                    var j = i;
+                    if (i == up.Length - 1)
+                    {
+                        up[i] = ' ';
+                    }
+                    else
+                    {
+                        while (j < up.Length - 1 && up[j] != ' ')
+                        {
+                            up[j] = up[j + 1];
+                            j++;
+                        }
+
+                        if (j != a1.Length)
+                            up[j] = ' ';
+                    }
+
+                    i--;
+                }
+            }
+
+            if (b.Length < up.Length)
+            {
+                for (int i = b.Length; i < up.Length && (up[i].ToString() != up[i].ToString().ToUpper()); i++)
+                {
+                    up[i] = ' ';
+                }
+            }
+
+            string str1 = string.Empty;
+            for (int i = 0; i < up.Length; i++)
+            {
+                if (up[i] != ' ')
+                {
+                    str1 = str1 + up[i].ToString().ToUpper();
+                }
+            }
+
+            return str1 == b ? "YES" : "NO";
+        }
+
+        public static string abbreviation(string a, string b)
+        {
+            Queue<char> memo = new Queue<char>();
+            return abs(a, b, memo) ? "YES" : "NO";
+        }
+
+        //private static bool abs1(string a, string b, Queue<char> memo)
+        //{
+        //    int aMaxIndex = a.Length - 1;
+        //    int bMaxIndex = b.Length - 1;
+
+        //    if (string.IsNullOrWhiteSpace(a) && string.IsNullOrWhiteSpace(b))
+        //        return true;
+
+        //    if (!string.IsNullOrWhiteSpace(b) && string.IsNullOrWhiteSpace(a))
+        //        return false;
+
+        //    if (string.IsNullOrWhiteSpace(b) && !string.IsNullOrWhiteSpace(a))
+        //    {
+        //        var pop = memo.Count > 0 ? memo.Dequeue() : ' ';
+        //        if (IsLowerCase(a[aMaxIndex]))
+        //        {
+        //            if (pop == a[aMaxIndex])
+        //            {
+        //                memo.Enqueue(a[aMaxIndex]);
+        //            }
+
+        //            //memo.Push(pop);
+        //            return abs(a.Substring(0, aMaxIndex), b, memo);
+        //        }
+        //        else
+        //        {
+        //            // If a[maxIndex] matches keyValue
+        //            if (pop.ToString() == a[aMaxIndex].ToString().ToLower())
+        //            {
+        //                return abs(a.Substring(0, aMaxIndex), b, memo);
+        //            }
+
+        //            return false;
+        //        }
+        //    }
+
+        //    if (b[bMaxIndex] == a[aMaxIndex])
+        //    {
+        //        return abs(a.Substring(0, aMaxIndex), b.Substring(0, bMaxIndex), memo);
+        //    }
+
+        //    if (b[bMaxIndex].ToString() == a[aMaxIndex].ToString().ToUpper())
+        //    {
+        //        memo.Enqueue(a[aMaxIndex]);
+        //        return abs(a.Substring(0, aMaxIndex), b.Substring(0, bMaxIndex), memo);
+        //    }
+
+        //    if (b[bMaxIndex] != a[aMaxIndex])
+        //    {
+        //        var pop = memo.Count > 0 ? memo.Dequeue() : ' ';
+        //        if (IsLowerCase(a[aMaxIndex]))
+        //        {
+        //            //if (pop == a[aMaxIndex])
+        //            //{                        
+        //            //    memo.Enqueue(a[aMaxIndex]);                        
+        //            //}
+
+        //            memo.Enqueue(a[aMaxIndex]);
+        //            //memo.Push(pop);
+        //            return abs(a.Substring(0, aMaxIndex), b, memo);
+        //        }
+        //        else
+        //        {
+        //            // If a[maxIndex] matches keyValue
+        //            if (pop.ToString() == a[aMaxIndex].ToString().ToLower())
+        //            {
+        //                return abs(a.Substring(0, aMaxIndex), b, memo);
+        //            }
+
+        //            return false;
+        //        }
+        //    }
+
+        //    return false;
+
+        //}
+
+        private static bool abs(string a, string b, Queue<char> memo)
+        {
+            int aMaxIndex = a.Length - 1;
+            int bMaxIndex = b.Length - 1;
+
+            #region Base-cases
+            //both empty
+            if (string.IsNullOrWhiteSpace(a) && string.IsNullOrWhiteSpace(b))
+                return true;
+
+            //a is empty but b have items
+            if (string.IsNullOrWhiteSpace(a) && !string.IsNullOrWhiteSpace(b))
+                return false;
+            #endregion
+
+            #region Need to re-visit -- special case 1
+            //b is empty and a is non-empty
+            if (string.IsNullOrWhiteSpace(b) && !string.IsNullOrWhiteSpace(a))
+            {
+                if (!IsLowerCase(a[aMaxIndex]))
+                {
+                    if (memo.Count == 0)
+                        return false;
+
+                    while (memo.Count > 0)
+                    {
+                        var deq = memo.Dequeue();
+
+                        if (deq.ToString().ToUpper() == a[aMaxIndex].ToString().ToUpper())
+                            break;
+                    }
+                }
+
+                return abs(a.Substring(0, aMaxIndex), b, memo); ;
+            }
+            #endregion
+
+            // Both same and upper case
+            if (a[aMaxIndex] == b[bMaxIndex])
+            {
+                //var foundinmemo = false;
+                //while (memo.Count > 0)
+                //{
+                //    var deq = memo.Dequeue();
+
+                //    if (deq.ToString().ToUpper() == a[aMaxIndex].ToString().ToUpper())
+                //    {
+                //        foundinmemo = true;
+                //        break;
+                //    }
+                //}
+
+                //if (foundinmemo)
+                //{
+                //    return abs(a.Substring(0, aMaxIndex), b, memo);
+
+                //}
+                //else
+                { return abs(a.Substring(0, aMaxIndex), b.Substring(0, bMaxIndex), memo); }
+            }
+
+            #region - special case 2
+            //both upper case and different - special case
+            if (IsUpperCase(a[aMaxIndex]) && a[aMaxIndex] != b[bMaxIndex])
+            {
+                if (memo.Count == 0)
+                    return false;
+
+                while (memo.Count > 0)
+                {
+                    var deq = memo.Dequeue();
+
+                    if (deq.ToString().ToUpper() == a[aMaxIndex].ToString().ToUpper())
+                        break;
+                }
+
+                return abs(a.Substring(0, aMaxIndex), b, memo);
+            }
+            #endregion
+
+            //a lower case and not equal to b
+            if (IsLowerCase(a[aMaxIndex]) && a[aMaxIndex].ToString().ToUpper() != b[bMaxIndex].ToString())
+            {
+                return abs(a.Substring(0, aMaxIndex), b, memo);
+            }
+
+            #region - special case 3
+            //a lower case and equal to b
+            if (IsLowerCase(a[aMaxIndex]) && a[aMaxIndex].ToString().ToUpper() == b[bMaxIndex].ToString())
+            {
+                //Memorize this
+                memo.Enqueue(a[aMaxIndex]);
+                return abs(a.Substring(0, aMaxIndex), b.Substring(0, bMaxIndex), memo);
+            }
+            #endregion
+
+            return false;
+        }
+
+        private static bool IsLowerCase(char c)
+        {
+            if (c >= 97 && c <= 122)
+                return true;
+
+            return false;
+        }
+
+        private static bool IsUpperCase(char c)
+        {
+            if (c >= 65 && c <= 90)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ DynamicProgramming/LongestCommonSubSequence.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class LongestCommonSubSequence
+    {
+        /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+        static public int lcsold(char[] X, char[] Y, int m, int n)
+        {
+            if (m == 0 || n == 0)
+                return 0;
+            if (X[m - 1] == Y[n - 1])
+                return 1 + lcs(X, Y, m - 1, n - 1);
+            else
+                return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
+        }
+
+        /* Utility function to get max of 2 integers */
+        static int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        static public int lcs(char[] X, char[] Y, int m, int n)
+        {
+            int[][] L = new int[m + 1][];
+            int i, j;
+
+            /* Following steps build L[m+1][n+1] in bottom up fashion. Note 
+               that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+            for (i = 0; i <= m; i++)
+            {
+                L[i] = new int[n + 1];
+                for (j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        L[i][j] = 0;
+
+                    else if (X[i - 1] == Y[j - 1])
+                        L[i][j] = L[i - 1][j - 1] + 1;
+
+                    else
+                        L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+                }
+            }
+
+            /* L[m][n] contains length of LCS for X[0..n-1] and Y[0..m-1] */
+            return L[m][n];
+        }
+    }
+}
